4b4b0b8e8e7d8c3bb369e0b4a83bb83f346ae437
Peter Varsanyi <peter.varsanyi@gwelican.eu>
2026-02-25 19:42:20 -0800

    feat(display): Wayland + Sway compositor

--- hosts/rpi-kiosk.nix
+++ hosts/rpi-kiosk.nix
@@ -3,6 +3,10 @@
   sops.age.keyFile = ./secrets/age.key;
   networking.hostName = "rpi-kiosk";
   services.dbus.enable = true;
+  hardware.raspberry-pi."5".base = true;
+  hardware.raspberry-pi."5".page-size-16k = true;
+  hardware.raspberry-pi."5".display-vc4 = true;
+  hardware.raspberry-pi."5".boot.loader = true;
   fileSystems."/" = {
     device = "/dev/mmcblk0p2";
     fsType = "ext4";
@@ -12,11 +16,15 @@
     devices = [ "/dev/mmcblk0" ];
   };
   system.stateVersion = "24.11";
+  services.xserver.enable = false;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm.wayland = true;
+  services.xserver.desktopManager.sway.enable = true;
+  programs.sway.enable = true;
   comin.systemd.enable = true;
-}
-
   services.openssh.enable = true;
   services.openssh.settings = {
     PermitRootLogin = "prohibit-password";
     PasswordAuthentication = false;
   };
+}
